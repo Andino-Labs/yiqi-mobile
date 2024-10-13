fa3b2da055aa863b355749071076b89a49df9aff
Paul Garcia <pbrain19@gmail.com>
2024-10-12 23:00:46 -0500

    adjustmets

---
+++ app/private/index.tsx
@@ -0,0 +1,4 @@
+// in this task create a main view page.
+
+// in this view I should be able to have quick access to my tickets for registered events, upcoming events & recommended events as well as notifications.
+// A dashboard of different types that is neede for a user.
